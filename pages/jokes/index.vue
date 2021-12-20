21aac495ed09f405758758e74cd4377e0c60b37f
OwerHub <owerwild@gmail.com>
2021-12-20 15:23:29 +0100

    fetch jokes and sent to componenet OK

--- pages/jokes/index.vue
+++ pages/jokes/index.vue
@@ -1,7 +1,54 @@
 <template>
-  <div>this is an jokes index page</div>
+  <div>
+    <Joke
+      v-for="joke in jokes"
+      :key="joke.id"
+      :id="joke.id"
+      :joke="joke.joke"
+    ></Joke>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import Joke from "../../components/Joke";
+
+export default {
+  comments: { Joke },
+  data() {
+    return {
+      jokes: [],
+    };
+  },
+
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+
+    try {
+      const url = "https://icanhazdadjoke.com/search";
+      const res = await axios.get(url, config);
+
+      this.jokes = res.data.results;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  head() {
+    return {
+      title: "Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place corny dad jokes",
+        },
+      ],
+    };
+  },
+};
 </script>
