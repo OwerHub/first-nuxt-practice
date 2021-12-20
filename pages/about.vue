a3efa4849514cd2ad0818def5d790b1f03e14c52
OwerHub <owerwild@gmail.com>
2021-12-20 12:08:15 +0100

    Some Page is OK

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>This is About Page</h1>
+    <p>this is an app that displays corny dad jokes</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About The App",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place corny dad jokes",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ pages/jokes/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>this is an jokes index page</div>
+</template>
+
+<script>
+export default {};
+</script>
